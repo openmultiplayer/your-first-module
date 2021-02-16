b41ea2504d31fb182bc3838f7ee7da17efb163e6
Y-Less <git@y-less.com>
2021-02-16 20:50:53 +0100

    Tutorial Step 6.
    Renamed native.
    
    # Conflicts:
    #	docs/guides/modules/RealWeather/Controller.cpp
    #	docs/guides/modules/RealWeather/Controller.hpp

---
+++ modules/RealWeather/Scripting.cpp
@@ -0,0 +1,29 @@
+// Include the controller, which is where the data being manipulated lives.
+#include "Controller.hpp"
+
+// Include the scripting system, for the `SCRIPT_API` wrapper and more.
+#include <open.mp/Server/Scripting.hpp>
+
+// Define an external interface to this module.  The PAWN language provider converts an output
+// string to a destination array and length, so this would be referenced as:
+//
+//     native RWW_GetCurrentWeather(string:output[], length = sizeof (output));
+//
+// And used as:
+//
+//    new weather[32];
+//    RWW_GetCurrentWeather(weather);
+//    printf("The current real-world weather is: %s", weather);
+//
+// The name is `RWW_GetCurrentWeather`, and the return type is `void`.  Out parameters are always
+// passed as pointers, to differentiate them to the marshalling templates.
+SCRIPT_API(RWW_GetCurrentWeather, void (std::string * output))
+{
+	// Get the global (singleton) instance of the `RealWorldController` module.
+	RealWorldController &
+		controller = RealWorldController::Instance();
+
+	// Call the method on the controller that gets the internal weather data.
+	*output = controller.GetCurrentWeather();
+}
+
