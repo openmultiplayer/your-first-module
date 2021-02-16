f26b974cac9857e9fec279c0979ae13c66322201
Y-Less <git@y-less.com>
2021-02-16 18:49:06 +0100

    Tutorial Step 3.
    # Conflicts:
    #	docs/guides/modules/RealWeather/Controller.cpp
    #	docs/guides/modules/RealWeather/Controller.hpp
    
    # Conflicts:
    #	docs/guides/modules/RealWeather/Controller.cpp
    #	docs/guides/modules/RealWeather/Controller.hpp

--- modules/RealWeather/Controller.cpp
+++ modules/RealWeather/Controller.cpp
@@ -10,6 +10,12 @@
 // Imaginary real world weather lookup library.
 #include <imaginary-real-world-weather-lookup-library>
 
+// To subscribe to the `OnPlayerConnect` event, a definition of the event is needed.
+#include <open.mp/Events/OnPlayerConnect.hpp>
+
+// The event is then marked as "required", to be checked at compile-time (unlike optional events).
+REQUIRED_EVENT(OnPlayerConnect);
+
 // constructor
 	RealWeatherController::
 	RealWeatherController()
@@ -17,24 +23,34 @@
 	// Pass a human-friendly name for this module through to the parent constructor.
 	SingletonModule<RealWeatherController>("Real Weather")
 {
-	std::cout << "Real World Weather module: v0.2" << std::endl;
+	std::cout << "Real World Weather module: v0.3" << std::endl;
 
 	// Get the weather for "Malta" from the real-world lookup system.
-	std::string
-		currentWeather = LookUpRealWorldWeather("Malta");
+	currentWeather_ = LookUpRealWorldWeather("Malta");
 
-	// Create a packet to send to all players.
-	SetWeatherPacket
-		weatherPacket {
-			// Due to a limitation in how C++ structs derive, all packet structures start with `{}`.
-			{},
+	// There is no longer any need to send the weather in the constructor.  There are no players.
 
-			// Convert from the string name of weather to a San Andreas weather type.
-			ConvertWeatherToID(currentWeather),
-		};
+	// Instead, subscribe to the `OnPlayerConnect` event, passing the name of the event and the
+	// callback (method) to be called every time the event publishes.  Send from there instead.
+	On(::OnPlayerConnect, &RealWeatherController::OnPlayerConnect);
+}
 
-	// Now send the packet to all players, telling their games to change their weather.
-	weatherPacket.SendToAll();
+// Define the method called every time a player connects and the `OnPlayerConnect` event fires.
+bool
+	RealWeatherController::
+	OnPlayerConnect(openmp::Player_s player)
+{
+	// Create an anonymous packet and send to the one player that just connected.
+	SetWeatherPacket {
+		// Due to a limitation in how C++ structs derive, all packet structures start with `{}`.
+		{},
+
+		// Convert from the string name of weather to a San Andreas weather type.
+		ConvertWeatherToID(currentWeather_),
+	}.SendTo(player);
+
+	// Doesn't matter.
+	return true;
 }
 
 // Common APIs will not return the current weather as an ID that the game will understand.  This
