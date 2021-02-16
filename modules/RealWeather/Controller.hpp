d00354c903d0ab160c5307b06c3f1265f1a8f6a0
Y-Less <git@y-less.com>
2021-02-16 18:49:06 +0100

    Tutorial Step 3.
    # Conflicts:
    #	docs/guides/modules/RealWeather/Controller.cpp
    #	docs/guides/modules/RealWeather/Controller.hpp
    
    # Conflicts:
    #	docs/guides/modules/RealWeather/Controller.cpp
    #	docs/guides/modules/RealWeather/Controller.hpp

--- modules/RealWeather/Controller.hpp
+++ modules/RealWeather/Controller.hpp
@@ -3,6 +3,9 @@
 // All modules derive from `BaseModule`, included transitively here.
 #include <open.mp/Module.hpp>
 
+// Include the basic definition of a player, as the code now needs to reference individuals.
+#include <open.mp/Player.hpp>
+
 // The main controller class for this module.
 class RealWeatherController
 	// Since there is only one instance of this module, it derives from `SingletonModule` with CRTP.
@@ -15,5 +18,12 @@ public:
 private:
 	// Because the API returns weather names as strings, this function converts them to game IDs.
 	int ConvertWeatherToID(std::string const & weatherName);
+
+	// When a player connects this module is informed.  Declare the method used for this.
+	bool OnPlayerConnect(openmp::Player_s player);
+
+	// Remember the name of the real-world weather, to be used repeatedly.
+	std::string
+		currentWeather_;
 };
 
